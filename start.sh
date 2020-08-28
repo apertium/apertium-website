077c6ad08c18735717d6cf67eaa4d6d0869b34c9
Tino Didriksen <mail@tinodidriksen.com>
2020-08-28 12:59:57 +0200

    Require Transfuse; Set memory limit

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --restart=unless-stopped -v /home/beta/logs:/home/apertium/logs -p 2738:2737 --name apertium-beta apertium-beta
+docker run -d --restart=unless-stopped --memory 20480m --memory-swap 30720m -v /home/beta/logs:/home/apertium/logs -p 2738:2737 --name apertium-beta apertium-beta
