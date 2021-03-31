0dff87b275b42ad7012b349d3ab115ed5c55c1cc
Tino Didriksen <mail@tinodidriksen.com>
2021-03-31 14:26:35 +0200

    Require gawk; Also build html-tools

--- cron.sh
+++ cron.sh
@@ -2,6 +2,7 @@
 set -e
 set -o pipefail
 
+# Docker image
 df -h /var/lib/docker
 time ./upgrade.sh
 docker stop apertium-beta
@@ -9,3 +10,10 @@ docker rm apertium-beta
 ./start.sh
 docker system prune -f
 df -h /var/lib/docker
+
+# Website
+cd ~beta/apertium-html-tools
+git pull --all --autostash --rebase
+make clean
+make
+rsync -avc --delete build/ ../public_html/
