d9aaccfa11a23e060757dd5cb45840b89125b038
Tino Didriksen <mail@tinodidriksen.com>
2025-02-20 12:31:31 +0000

    jammy -> noble; Squash differently

--- beta/install.sh
+++ beta/install.sh
@@ -3,5 +3,5 @@ export DOCKER_BUILDKIT=1
 export BUILDKIT_PROGRESS=plain
 export PROGRESS_NO_TRUNC=1
 
-docker build --squash --pull -t apertium-beta -f Dockerfile.install .
+docker build --pull -t apertium-beta -f Dockerfile.install .
 ./upgrade.sh
--- release/upgrade.sh
+++ release/upgrade.sh
@@ -3,4 +3,4 @@ export DOCKER_BUILDKIT=1
 export BUILDKIT_PROGRESS=plain
 export PROGRESS_NO_TRUNC=1
 
-docker build --squash --no-cache -t apertium-release -f Dockerfile.upgrade .
+docker build --no-cache -t apertium-release -f Dockerfile.upgrade .
